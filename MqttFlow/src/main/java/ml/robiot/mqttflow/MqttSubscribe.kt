6edaeabb8c47233865c1f3f29b2b947b166dc6ee
Hugo Cruz <hugocuzanillo@gmail.com>
2022-12-31 02:26:41 -0600

    Second commit

--- MqttFlow/src/main/java/ml/robiot/mqttflow/MqttSubscribe.kt
+++ MqttFlow/src/main/java/ml/robiot/mqttflow/MqttSubscribe.kt
@@ -1,22 +1,35 @@
 package ml.robiot.mqttflow
 
-class MqttSubscribe /*@Inject constructor(private val mqttClient: MqttAndroidClient) {
+import android.util.Log
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.channels.onFailure
+import kotlinx.coroutines.channels.trySendBlocking
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import org.eclipse.paho.android.service.MqttAndroidClient
+import org.eclipse.paho.client.mqttv3.IMqttActionListener
+import org.eclipse.paho.client.mqttv3.IMqttToken
+import javax.inject.Inject
+
+class MqttSubscribe @Inject constructor(private val mqttClient: MqttAndroidClient) {
     companion object {
-        const val TAG_SUBSCRIBE = "MqttSubscribe"
+        private const val TAG = "MqttSubscribe"
     }
     suspend operator fun invoke(topic: String, qos: Int): Flow<Boolean> = callbackFlow {
         val iMqttActionListener =  object : IMqttActionListener {
             override fun onSuccess(asyncActionToken: IMqttToken?) {
-                Log.d(TAG_SUBSCRIBE, "The subscription was successful")
-                trySend(true)
+                trySendBlocking(true).onFailure {
+                    Log.d(TAG, "On Failure by: $it")
+                }
             }
 
             override fun onFailure(asyncActionToken: IMqttToken?, exception: Throwable?) {
-                Log.d(TAG_SUBSCRIBE, "Connect failed with exception: $exception")
-                trySend(false)
+                trySendBlocking(false).onFailure {
+                    Log.d(TAG, "On Failure by: $it")
+                }
             }
         }
         val subscribe = mqttClient.subscribe(topic, qos, null, iMqttActionListener)
         awaitClose { subscribe.isComplete }
     }
-}*/
+}
